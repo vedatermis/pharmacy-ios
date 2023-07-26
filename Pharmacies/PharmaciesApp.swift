0c30256eaeaa6ae66b9f9f0a4b625a1082b3a8e5
Vedat ERMİŞ <vedatermis@hotmail.com>
2023-07-26 14:21:48 +0300

    Initial Commit

---
+++ Pharmacies/PharmaciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmaciesApp.swift
+//  Pharmacies
+//
+//  Created by Vedat ERMIS on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct PharmaciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
